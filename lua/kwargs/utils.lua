0468db593d4c171b629f12125b4928a8419b80a2
Eric Laufer <eric@reliant.ai>
2024-12-27 21:31:30 -0500

    Unused

--- lua/kwargs/utils.lua
+++ lua/kwargs/utils.lua
@@ -12,19 +12,4 @@ M.maybe_print = function(x, debug)
     end
 end
 
---- Returns true if the string contains an equal sign outside of parentheses.
----@param s string
----@return boolean
-M.contains_equal_outside_of_parentheses = function(s)
-    for i = 1, #s do
-        local char = s:sub(i, i)
-        if char == "=" then
-            return true
-        elseif char == "(" then
-            return false
-        end
-    end
-    return false
-end
-
 return M
