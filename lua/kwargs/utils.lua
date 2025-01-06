551ec121be13fbe53eafaea58ea8f4441cdd3145
Eric Laufer <eric@reliant.ai>
2025-01-05 21:08:12 -0500

    Oops

--- lua/kwargs/utils.lua
+++ lua/kwargs/utils.lua
@@ -12,4 +12,41 @@ M.maybe_print = function(x, debug)
     end
 end
 
+-- --- Returns true if the string contains an equal sign outside of parentheses.
+-- ---@param s string
+-- ---@return boolean
+-- M.contains_equal_outside_of_parentheses = function(s)
+--     for i = 1, #s do
+--         local char = s:sub(i, i)
+--         if char == "=" then
+--             return true
+--         elseif char == "(" then
+--             return false
+--         end
+--     end
+--     return false
+-- end
+
+---BFS on the tree under `node` to find the first node of type `target_type`
+---@param node TSNode
+---@param target_type string
+---@return TSNode
+M.find_first_node_of_type_bfs = function(node, target_type)
+    local queue = { node }
+
+    while #queue > 0 do
+        local current_node = table.remove(queue, 1) -- Dequeue the first element
+
+        if current_node:type() == target_type then
+            return current_node
+        end
+
+        for child in current_node:iter_children() do
+            table.insert(queue, child) -- Enqueue child nodes
+        end
+    end
+
+    error("Node not found")
+end
+
 return M
